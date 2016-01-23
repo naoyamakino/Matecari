91ceef5034c38560a4b39dfc1a10409f226f8acb
Naoya Makino <naoyamakino@gmail.com>
2016-01-23 13:01:15 -0800

    added view pager fragment

---
+++ app/src/main/java/com/naoya/matecari/ui/MainActivity.java
@@ -0,0 +1,102 @@
+package com.naoya.matecari.ui;
+
+import com.naoya.matecari.R;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.tabLayout)
+    TabLayout mTabLayout;
+
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mTabLayout.addTab(mTabLayout.newTab().setText(getString(R.string.men)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(getString(R.string.all)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(getString(R.string.women)));
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(viewPagerAdapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+
+    static private class ViewPagerAdapter extends FragmentPagerAdapter {
+        public ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new MenPagerFragment();
+                case 1:
+                    return new AllPagerFragment();
+                case 2:
+                    return new WomenPagerFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+}
---
+++ app/src/main/java/com/naoya/matecari/ui/MenPagerFragment.java
@@ -0,0 +1,10 @@
+package com.naoya.matecari.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Naoya on 16-01-23.
+ */
+public class MenPagerFragment extends Fragment {
+
+}
