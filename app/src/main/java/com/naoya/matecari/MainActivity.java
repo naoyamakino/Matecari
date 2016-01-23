1fe2dd36da80eabed0f843e23f49e9b43432e951
Naoya Makino <naoyamakino@gmail.com>
2016-01-23 12:47:43 -0800

    added tabLayout

--- app/src/main/java/com/naoya/matecari/MainActivity.java
+++ app/src/main/java/com/naoya/matecari/MainActivity.java
@@ -3,20 +3,35 @@ package com.naoya.matecari;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.tabLayout)
+    TabLayout mTabLayout;
+
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mTabLayout.addTab(mTabLayout.newTab().setText(getString(R.string.men)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(getString(R.string.all)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(getString(R.string.women)));
+
     }
 
     @Override
